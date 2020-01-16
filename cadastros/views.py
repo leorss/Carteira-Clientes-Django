d92dbdb27463b7bb1d114fc4ebc1774d91ae273f
Leo Ribeiro e Silva <leorss@gmail.com>
2020-01-15 18:32:01 -0600

    Adicionado botao anterior/ultimo paginacao

--- cadastros/views.py
+++ cadastros/views.py
@@ -6,10 +6,14 @@ from django.core.paginator import Paginator, InvalidPage, EmptyPage
 def index(request):
     return render(request, 'cadastros/index.html')
 
-def Vendedores(request):
 
+def Http404(request, msg):
+    return render(request, 'cadastros/404.html')
+
+
+def Vendedores(request):
     lista = Vendedor.objects.order_by('id')
-    paginator = Paginator(lista, 10) # Mostra 25 contatos por página
+    paginator = Paginator(lista, 5) # Mostra 25 contatos por página
 
     # Make sure page request is an int. If not, deliver first page.
     # Esteja certo de que o `page request` é um inteiro. Se não, mostre a primeira página.
@@ -28,9 +32,8 @@ def Vendedores(request):
 
 
 def Clientes(request):
-
     lista = Cliente.objects.order_by('id')
-    paginator = Paginator(lista, 10) # Mostra 25 contatos por página
+    paginator = Paginator(lista, 5) # Mostra 25 contatos por página
 
     # Make sure page request is an int. If not, deliver first page.
     # Esteja certo de que o `page request` é um inteiro. Se não, mostre a primeira página.
@@ -55,7 +58,6 @@ def ClienteDetalhe(request, pk):
         raise Http404('Id inválido')
 
     return render(request, 'cadastros/clientes_detalhe.html', context={'detalhe': detalhe})
-
 
 def VendedorDetalhe(request, pk):
     try:
